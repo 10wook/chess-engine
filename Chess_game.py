111e9d29b404de26125def9c217e7fffd2a4d12a
10wook <younguk137@naver.com>
2022-12-30 12:45:09 +0900

    pygame study

--- Chess_game.py
+++ Chess_game.py
@@ -3,11 +3,12 @@ import sys
 from pygame.locals import *
 pygame.init()
 
-width = 600
-height = 600
+width = 633
+height = 640
 black = (0,0,0)
 pygame.display.set_caption("let's play chess!!")
 main_display = pygame.display.set_mode((width,height),0,32) 
+board = pygame.image.load("./Chess_Board.gif")
 gulimfont = pygame.font.SysFont('굴림',70)
 start_chess = gulimfont.render("let's play chess!!",1,black)
 hellorect = start_chess.get_rect()
@@ -19,8 +20,7 @@ while True:
             pygame.quit()
             sys.exit()
     main_display.fill((255,255,255))
-    main_display.blit(start_chess,hellorect)
-    
+    main_display.blit(board,(0,0))
     pygame.display.update()
     
             
