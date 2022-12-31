74612533da3ae65ab659e44ab5e5dee15a3fc5fb
10wook <younguk137@naver.com>
2022-12-31 17:59:52 +0900

    Luke func made

--- piece_maker.py
+++ piece_maker.py
@@ -4,6 +4,16 @@ from pygame.locals import *
 
 
 def pawn_maker(P,board,color,place):
-    P = pygame.image.load("./WP.gif")
+    if color == 1:#하얀색이라는 뜻
+        P = pygame.image.load("./WP.gif")
+    else:
+        P = pygame.image.load("./BP.gif")
+    board.blit(P,place)
+    return P
+def Luke_maker(P,board,color,place):
+    if color == 1:#하얀색이라는 뜻
+        P = pygame.image.load("./WL.gif")
+    else:
+        P = pygame.image.load("./BL.gif")
     board.blit(P,place)
     return P
