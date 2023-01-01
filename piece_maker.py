31e23d0b30547bf1823e112acf0e23081fe3ad7f
10wook <younguk137@naver.com>
2023-01-01 13:49:22 +0900

    func made

--- piece_maker.py
+++ piece_maker.py
@@ -17,3 +17,34 @@ def Luke_maker(P,board,color,place):
         P = pygame.image.load("./BL.gif")
     board.blit(P,place)
     return P
+def Knight_maker(P,board,color,place):
+    if color == 1:#하얀색이라는 뜻
+        P = pygame.image.load("./WN.gif")
+    else:
+        P = pygame.image.load("./BN.gif")
+    board.blit(P,place)
+    return P
+def Queen_maker(P,board,color,place):
+    if color == 1:#하얀색이라는 뜻
+        P = pygame.image.load("./WQ.gif")
+    else:
+        P = pygame.image.load("./BQ.gif")
+    board.blit(P,place)
+    return P
+def King_maker(P,board,color,place):
+    if color == 1:#하얀색이라는 뜻
+        P = pygame.image.load("./WK.gif")
+    else:
+        P = pygame.image.load("./BK.gif")
+    board.blit(P,place)
+    return P
+def Bishop_maker(P,board,color,place):
+    if color == 1:#하얀색이라는 뜻
+        P = pygame.image.load("./WB.gif")
+    else:
+        P = pygame.image.load("./BB.gif")
+    board.blit(P,place)
+    return P
+
+def pos_init():
+    return
